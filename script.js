ab404f7476e8846717f84b400a7375d5b126b142
5 Dakikada İngilizce <53emrepoyraz53@gmail.com>
2026-05-31 17:24:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const words = [];
+let words = JSON.parse(localStorage.getItem("words")) || [];
+
+renderWords();
 
 function addWord() {
 
@@ -9,23 +11,58 @@ const value = input.value.trim();
 if (!value) return;
 
 const wordData = {
+id: Date.now(),
 word: value,
-meaning: "loading...",
+meaning: "Loading meaning...",
 example: "AI example sentence will come here."
 };
 
-words.push(wordData);
+words.unshift(wordData);
+
+saveWords();
 
 renderWords();
 
 input.value = "";
 }
 
+function deleteWord(id) {
+
+words = words.filter((item) => item.id !== id);
+
+saveWords();
+
+renderWords();
+}
+
+function saveWords() {
+
+localStorage.setItem(
+"words",
+JSON.stringify(words)
+);
+}
+
 function renderWords() {
 
-const wordList = document.getElementById("wordList");
+const wordList =
+document.getElementById("wordList");
 
 wordList.innerHTML = "";
+
+if (words.length === 0) {
+
+```
+wordList.innerHTML = `
+  <div class="empty-state">
+    No words added yet.
+  </div>
+`;
+
+return;
+```
+
+}
 
 words.forEach((item) => {
 
@@ -33,7 +70,18 @@ words.forEach((item) => {
 wordList.innerHTML += `
   <div class="word-card">
 
-    <h2>${item.word}</h2>
+    <div class="card-top">
+
+      <h2>${item.word}</h2>
+
+      <button
+        class="delete-btn"
+        onclick="deleteWord(${item.id})"
+      >
+        ✕
+      </button>
+
+    </div>
 
     <p>${item.meaning}</p>
 
